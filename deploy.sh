31fd7e02ec1ffb707dbb643c3e4c1abf303bbf9e
Daniel Koo <danieljamin.koo@gmail.com>
2014-01-14 09:55:23 -0500

    Adding script to handle deployments for pull requests
    Signed-off-by: Daniel Koo <danieljamin.koo@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+mvn install --settings target/CM/settings.xml
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
+#
+# Ignore pull requests
+if [ $TRAVIS_SECURE_ENV_VARS == 'true' ]; then
+echo "Attempting deployment..."
+
+if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
+  echo "Not a pull request, deploying..."
+  mvn deploy --settings target/travis/settings.xml
+
+fi
+fi
