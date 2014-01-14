4f4f09e4c5fda791bb3be40904f290d680dd99da
Daniel Koo <danieljamin.koo@gmail.com>
2014-01-14 10:56:58 -0500

    Modified logic to run either install or deploy
    Signed-off-by: Daniel Koo <danieljamin.koo@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-mvn install --settings target/CM/settings.xml
 if [ $? -ne 0 ]; then
   exit -1
 fi
@@ -11,6 +10,11 @@ echo "Attempting deployment..."
 if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   echo "Not a pull request, deploying..."
   mvn deploy --settings target/CM/settings.xml
-
+else
+  echo "Pull request, installing..."
+  mvn install --settings target/CM/settings.xml
 fi
+else
+  echo "No deployment necessary, installing..."
+  mvn install --setings target/CM/settings.xml
 fi
