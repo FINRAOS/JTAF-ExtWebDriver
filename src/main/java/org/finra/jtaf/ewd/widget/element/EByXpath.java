d55b537455ef86112c1a2585180f96a54d6a63f1
niels <opensource21@gmail.com>
2014-08-12 20:27:01 +0200

    [#75] Fix JavaDoc
    Signed-off-by: niels <opensource21@gmail.com>

--- src/main/java/org/finra/jtaf/ewd/widget/element/EByXpath.java
+++ src/main/java/org/finra/jtaf/ewd/widget/element/EByXpath.java
@@ -2,6 +2,11 @@ package org.finra.jtaf.ewd.widget.element;
 
 import org.openqa.selenium.By;
 
+/**
+ * {@link StringLocatorAwareBy} implemantation of {@link ByXPath}.
+ * @author niels
+ *
+ */
 public class EByXpath extends StringLocatorAwareBy {
     
     public EByXpath(String locator) {
